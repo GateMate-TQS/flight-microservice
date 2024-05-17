2a57893aadcef43236b2bc4d28370d2476529196
andreaoliveira9 <andreaoliveira@ua.pt>
2024-05-17 15:22:12 +0100

    SCRUM-29 done

--- api/src/main/java/gatemate/services/FlightServiceImpl.java
+++ api/src/main/java/gatemate/services/FlightServiceImpl.java
@@ -2,17 +2,13 @@ package gatemate.services;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.JsonNode;
 
 import gatemate.entities.Flight;
 import gatemate.repositories.FlightRepository;
 
 @Service
 public class FlightServiceImpl implements FlightService {
-  @Autowired
   private final FlightRepository flightRepository;
 
   public FlightServiceImpl(FlightRepository flightRepository) {
@@ -26,23 +22,5 @@ public class FlightServiceImpl implements FlightService {
         .filter(flight -> to == null || flight.getDestination().getIata().equals(to))
         .filter(flight -> flightIata == null || String.valueOf(flight.getFlightIata()).equals(flightIata))
         .toList();
-  }
-
-  @Override
-  public Flight getFlightInfo(String flightIata) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getFlightInfo'");
-  }
-
-  @Override
-  public JsonNode fetchFlightInfo(String flightIata) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'fetchFlightInfo'");
-  }
-
-  @Override
-  public Flight purchaseTicket(String flightIata, String seatClass, int numberOfTickets) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'purchaseTicket'");
   }
 }
