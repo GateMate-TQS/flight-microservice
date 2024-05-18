6a94aab0837732e2666914aee5a71213774191da
andreaoliveira9 <andreaoliveira@ua.pt>
2024-05-18 01:08:00 +0100

    SCRUM-26 removed useless import

--- api/src/main/java/gatemate/controllers/FlightController.java
+++ api/src/main/java/gatemate/controllers/FlightController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import gatemate.consumer.FlightDataConsumer;
 import gatemate.entities.Flight;
 import gatemate.services.FlightService;
 
